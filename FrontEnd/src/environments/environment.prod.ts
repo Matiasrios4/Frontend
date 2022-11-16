6ad3bd83e5ad7bb0297180012a6ebe55bd877ee2
Matiasrios4 <matiarios4@hotmail.es>
2022-11-16 17:10:36 -0300

    acercade y hys

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'frontendapti',
+    appId: '1:333524927827:web:7d77694bd7a5dfa9148da6',
+    storageBucket: 'frontendapti.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyD0KmVyXXZJaEa5c76V_UTZcFUGig0ySO8',
+    authDomain: 'frontendapti.firebaseapp.com',
+    messagingSenderId: '333524927827',
+  },
+  production: true,
+  URL:'https://backendapti.herokuapp.com/'
 };
--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'frontendapti',
+    appId: '1:333524927827:web:7d77694bd7a5dfa9148da6',
+    storageBucket: 'frontendapti.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyD0KmVyXXZJaEa5c76V_UTZcFUGig0ySO8',
+    authDomain: 'frontendapti.firebaseapp.com',
+    messagingSenderId: '333524927827',
+  },
+  production: false,
+
+  URL: 'http://localhost:8080/',
+ 
 };
 
 /*
